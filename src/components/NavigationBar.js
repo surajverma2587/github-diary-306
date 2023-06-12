393b50f46f466fa3d6f9059da36d8ff962e723c4
surajverma2587 <surajverma2587@gmail.com>
2023-06-12 20:08:27 +0100

    styled nav bar

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,5 +1,25 @@
-import Stack from "react-bootstrap/Stack";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
 
 export const NavigationBar = () => {
-  return <Stack>NavigationBar</Stack>;
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand>GitHub Diary</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav>
+            <Link to="/" className="nav-link">
+              Home
+            </Link>
+            <Link to="/my-repos" className="nav-link">
+              My Repos
+            </Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
